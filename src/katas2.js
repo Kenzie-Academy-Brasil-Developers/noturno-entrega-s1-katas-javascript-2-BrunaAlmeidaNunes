e328dbe216e13bb413e4e7b191ae9cff63527186
Bruna Almeida Nunes da Conceicao <brunamamamu@gmail.com>
2021-10-18 21:14:45 -0300

    add respostas

--- src/katas2.js
+++ src/katas2.js
@@ -1,36 +1,43 @@
 // comece a criar a sua função add na linha abaixo
 
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
-
+function add (numero1, numero2){
+    let soma = numero1 + numero2 
+    return soma
+}
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 // comece a criar a sua função multiply na linha abaixo
 
-
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
-
+function multiply(numero1, numero2) {
+    let resultado = 0
+    for (let contador = 0; contador < numero2; contador++) {
+        resultado = add(resultado, numero1)
+    }
+    return resultado
+}
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 // comece a criar a sua função power na linha abaixo
 
-
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
-
+function power(x, exponen) {
+    let resultado = 1
+    for (let contador = 0; contador < exponen; contador++) {
+        resultado = multiply(resultado, x)
+    }
+    return resultado
+}
+console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 // comece a criar a sua função factorial na linha abaixo
 
+function factorial(fatorial) {
+    let resultado = 1
+    for (let contador = fatorial; contador > 1; contador--) {
+        resultado = multiply(resultado, contador)
+    }
+    return resultado
+}
 
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
-
-/**
- * BONUS (aviso: o grau de dificuldade é bem maior !!!)
- */
-
-// crie a função fibonacci
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
+//Não consegui o bonus =(
